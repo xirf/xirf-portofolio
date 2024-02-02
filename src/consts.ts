61512e8bdec7a7cbf6361c0aae6eae82f37799fc
xirf <itsandka@gmail.com>
2024-02-02 23:53:48 +0700

    feat: refactor code

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Andka';
-export const SITE_DESCRIPTION = 'The place where I write about my projects and other stuff.';
