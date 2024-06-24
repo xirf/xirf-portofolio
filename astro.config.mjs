9a0d7e05800a7b000c6599452d34e0c77d5b5c5d
xirf <itsandka@gmail.com>
2024-06-24 16:48:54 +0700

    feat: server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import UnoCss from "unocss/astro"
+import UnoCss from "unocss/astro";
 import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://andka.my.id',
-	integrations: [
-		mdx(),
-		sitemap(),
-		UnoCss({
-			injectReset: true,
-		})
-	],
+  site: 'https://andka.my.id',
+  integrations: [mdx(), sitemap(), UnoCss({
+    injectReset: true
+  })],
+  output: "static", // this site don't need server
+  adapter: vercel()
 });
