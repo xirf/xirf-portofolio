d6e3c0661bef80bd90423b30180a28eeeccfa275
Restyled.io <commits@restyled.io>
2024-06-24 09:52:45 +0000

    Restyled by clang-format

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,27 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro"
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   pluginJs.configs.recommended,
   ...eslintPluginAstro.configs.all,
   ...tseslint.configs.recommended,
   {
-    files: [ "**/*.{js,mjs,cjs,ts,astro}" ],
-    rules: {
-      languageOptions: {
-        parserOptions: {
-          project: true,
-          extraFileExtension: [".astro"],
-          tsConfigRootDir: import.meta.dirname
+    files : [ "**/*.{js,mjs,cjs,ts,astro}" ],
+    rules : {
+      languageOptions : {
+        parserOptions : {
+          project : true,
+          extraFileExtension : [ ".astro" ],
+          tsConfigRootDir : import.meta.dirname
         }
       }
     }
   },
   {
-    languageOptions: {
-      globals: globals.browser,
+    languageOptions : {
+      globals : globals.browser,
     },
   },
 ];
